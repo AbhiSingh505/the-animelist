6c75feb2d89b839e8f10fde2f60c0dbc00ed9408
Abhishek Singh <chouhanabhishek421@gmail.com>
2025-08-16 11:35:39 +0530

    Initial Commit

---
+++ myanimelist/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  myanimelist
+//
+//  Created by Abhishek Singh Chouhan on 16/08/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ myanimelist/myanimelistApp.swift
@@ -0,0 +1,17 @@
+//
+//  myanimelistApp.swift
+//  myanimelist
+//
+//  Created by Abhishek Singh Chouhan on 16/08/25.
+//
+
+import SwiftUI
+
+@main
+struct myanimelistApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
